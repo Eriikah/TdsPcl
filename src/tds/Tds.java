116bdf287330d60f4b9638a4f9d92a9a8feb263a
PERONNIN Edgar <edgar.peronnin@telecomnancy.eu>
2022-12-26 16:40:55 +0100

    [add] classe Tds

--- src/tds/Tds.java
+++ src/tds/Tds.java
@@ -7,9 +7,19 @@ public class Tds {
     private int imbric;
     private ArrayList<Symbol> symbols;
 
-    public Tds(int imbric, ArrayList<Symbol> symbols) {
+    private Tds parent;
+    private ArrayList<Tds> enfants;
+    private String name;
+    private int bloc;
+
+    public Tds(int imbric, ArrayList<Symbol> symbols, int bloc) {
         this.imbric = imbric;
         this.symbols = symbols;
+
+        this.parent = null;
+        this.enfants = new ArrayList<>();
+        //this.name = name;
+        this.bloc = bloc;
     }
 
     public void addSymbol(Symbol symbol) {
@@ -22,5 +32,70 @@ public class Tds {
 
     public ArrayList<Symbol> getSymbols() {
         return symbols;
+    }
+
+    public ArrayList<Tds> getEnfants() {
+        return enfants;
+    }
+
+    public void addEnfant(Tds enfant) {
+        this.enfants.add(enfant);
+    }
+
+    public Tds getParent() {
+        return parent;
+    }
+
+    public void setParent(Tds parent) {
+        this.parent = parent;
+        this.parent.addEnfant(this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBloc() {
+        return bloc;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder table = new StringBuilder();
+        Tds pere = this.getParent();
+        if (pere != null) {
+            table.append(String.format("_ %s - Imbrication : %s - Bloc : %s - Parent : %s _\n", this.getName(), this.getImbric(), this.getBloc(), pere.getBloc()));
+        }
+        else {
+            table.append(String.format("_ %s - Imbrication : %s - Bloc : %s - Parent : null _\n", this.getName(), this.getImbric(), this.getBloc()));
+        }
+        for (Symbol symbol : this.getSymbols()) {
+            if (symbol instanceof Param){
+                Param param = (Param) symbol;
+                String ligne = String.format("Paramètre : %s // Type : %s // Déplacement : %d", param.getName(), param.getType(), param.getDepl());
+                table.append(ligne).append("\n");
+            }
+            else if (symbol instanceof Function){
+                Function function = (Function) symbol;
+                String ligne = String.format("Fonction : %s // Type de retour : %s // Déplacement : %d", function.getName(), function.getReturnType(), function.getParams());
+                if (function.getParams() != null) {
+                    for (Param param : function.getParams()) {
+                        ligne += String.format("Paramètre : %s // Type : %s // Déplacement : %d", param.getName(), param.getType(), param.getDepl());
+                    }
+                }
+                else {
+                    ligne += "Pas de paramètres";
+                }
+                
+                table.append(ligne).append("\n");
+            }
+            else if (symbol instanceof Var){
+                Var var = (Var) symbol;
+                String ligne = String.format("Variable : %s // Type : %s // Déplacement : %d", var.getName(), var.getType(), var.getDepl());
+                table.append(ligne).append("\n");
+            }
+        }
+        table.append("---END---\n");
+        return table.toString();
     }
 }
