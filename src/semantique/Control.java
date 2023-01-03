22f8ad97bd046f4edb05b7ea36c287a12ccf1374
PERONNIN Edgar <edgar.peronnin@telecomnancy.eu>
2023-01-03 21:42:57 +0100

    [add] package sémantique

---
+++ src/semantique/Control.java
@@ -0,0 +1,63 @@
+package semantique;
+
+import java.util.ArrayList;
+import tds.*;
+import ast.*;
+
+public class Control {
+    private Tds tds;
+    private Ast ast;
+    private ArrayList<Tds> tdsList;
+
+    public Control(Ast ast, Tds tds, ArrayList<Tds> tdsList) {
+        this.ast = ast;
+        this.tds = tds;
+        this.tdsList = tdsList;
+    }
+
+    public String getType(String name){
+        Tds tds_current = this.tds;
+
+        if (tds_current == null) {
+            System.out.println("Tds null");
+            return null;
+        }
+
+        Symbol symbol = tds_current.getSymbol(name);
+
+        while (symbol == null) {
+            tds_current = tds_current.getParent();
+            if (tds_current == null) {
+                System.out.println("Symbol null");
+                return null;
+            }
+            symbol = tds_current.getSymbol(name);
+        }
+
+        if (symbol instanceof Function){
+            return ((Function) symbol).getReturnType();
+        } 
+        else if (symbol instanceof Var){
+            return ((Var) symbol).getType();
+        } 
+        else if (symbol instanceof Param){
+            return ((Param) symbol).getType();
+        } 
+        else {
+            return null;
+        }
+    }
+
+    public Tds getTds(String name) {
+        for (Tds tds : this.tdsList) {
+            if (tds.getName().equals(name)) {
+                return tds;
+            }
+        }
+        return null;
+    }
+
+    public String getType(Ast ast){
+        //todo
+    }
+}
